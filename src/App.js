e4cacf7cc81dd2ed03ba9a2a991707e968b5a173
Sanjeevv30 <sanjeevsrivastava107@gmail.com>
2023-02-16 15:22:33 +0530

    Frontend

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
 
-function App() {
+const App = () => {
+  const [file, setFile] = useState(null);
+
+  const handleFileChange = e => {
+    setFile(e.target.files[0]);
+  };
+  const handleUpload = async () => {
+    const formData = new FormData();
+    formData.append("file", file);
+    try {
+      const res = await axios.post("http://localhost:5000/upload", formData);
+      console.log(res.data);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <input type="file" onChange={handleFileChange} />
+      <button onClick={handleUpload}>Upload</button>
     </div>
   );
-}
-
+};
 export default App;
+
+
+
